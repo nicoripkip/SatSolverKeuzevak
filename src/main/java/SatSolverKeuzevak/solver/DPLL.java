c2a4f09b2252fa419a5c844ed595216fcc28cb91
Nikoripkip <nicovanommen.nvo@gmail.com>
2023-10-25 15:27:58 +0200

    Unit propagate function added

--- src/main/java/SatSolverKeuzevak/solver/DPLL.java
+++ src/main/java/SatSolverKeuzevak/solver/DPLL.java
@@ -4,6 +4,10 @@ package SatSolverKeuzevak.solver;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import SatSolverKeuzevak.solver.Clause;
 
 
 /**
@@ -13,8 +17,8 @@ import java.util.Set;
  */
 public class DPLL
 {
-    private List<Clause>        _clauses;
-    private ArrayList<Integer>  _Model;
+    private Set<Clause>     _clauses;
+    private Set<Integer>    _Model;
 
 
     /**
@@ -30,18 +34,21 @@ public class DPLL
 
 
     /**
+     * Functie om het algoritme uit te voeren
      * 
-     * 
+     * @param phi
      * @return
      */
     public Boolean run(Set<Clause> phi)
     {
         while (this.containsUnitClause(phi)) {
-
+            int literal = getUnitLiteral(phi);
+            phi = this.unitPropagate(literal, phi);
         }
 
         while (this.containsPureLiteral(phi)) {
-
+            int literal = 0;
+            phi = this.pureLiteralAssign(literal, phi);
         }
 
         if (phi.isEmpty()) {
@@ -49,12 +56,18 @@ public class DPLL
         }
 
         if (this.containsEmptyClause(phi)) {
+            return false;
+        }   
 
-        }
+        int literal = this.chooseLiteral(phi);
+        Set<Clause> phi_p = phi;
+
+        phi.addAll(literal);
+
+        phi_p.addAll();
 
         // https://en.wikipedia.org/wiki/Short-circuit_evaluation
-        // return this.run() || this.run();
-        return false;
+        return this.run(phi) || this.run(phi_p);
     }
 
 
@@ -62,20 +75,48 @@ public class DPLL
      * Functie om unit clauses te verwijderen uit de lijst met clauses
      * https://en.wikipedia.org/wiki/Unit_propagation
      * 
+     * @param literal
+     * @param phi
      * @return
      */
-    private Set<Clause> unitPropagate()
+    private Set<Clause> unitPropagate(int literal, Set<Clause> phi)
     {
-        return new HashSet<Clause>();
+        Set<Clause> phi_p = new HashSet<Clause>();
+
+        for (Clause x : phi) {
+            Set<Integer> nLiterals = new HashSet<Integer>();
+            Boolean literalFound = false;
+            
+            for (int l : x.getLiterals()) {
+                if (l == literal) {
+                    literalFound = true;
+                    break;
+                }
+
+                if (l != -literal) {
+                    nLiterals.add(l);
+                }
+            }
+
+            if (!literalFound) {
+                Clause clause = new Clause();
+                clause.setLiterals(nLiterals);
+                phi_p.add(clause);
+            }
+        }
+
+        return phi_p;
     }
 
 
     /**
      * Functie om elke pure literal uit de lijst met clauses te halen
      * 
+     * @param literal
+     * @param phi
      * @return
      */
-    private Set<Clause> pureLiteralElimination()
+    private Set<Clause> pureLiteralAssign(int literal, Set<Clause> phi)
     {
         return new HashSet<Clause>();
     }
@@ -87,20 +128,79 @@ public class DPLL
      * @param clauses
      * @return
      */
-    private Boolean containsUnitClause(Set<Clause> clauses)
+    private Boolean containsUnitClause(Set<Clause> phi)
+    {
+        for (Clause x : phi) {
+            if (x.getLiterals().size() == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Functie om te checken of een literal pure is ja of te nee
+     * 
+     * @param phi
+     * @return
+     */
+    private Boolean containsPureLiteral(Set<Clause> phi)
     {
         return false;
     }
 
 
-    private Boolean containsPureLiteral(Set<Clause> clauses)
+    /**
+     * Functie om te checken of een clause leeg is ja of te nee
+     * 
+     * @param phi
+     * @return
+     */
+    private Boolean containsEmptyClause(Set<Clause> phi)
     {
+        for (Clause x : phi) {
+            if (x.getLiterals().size() == 0) {
+                return true;
+            }
+        }
+
         return false;
     }
 
 
-    private Boolean containsEmptyClause(Set<Clause> clauses)
+    /**
+     * Functie om een literal uit lijst phi te kiezen
+     * 
+     * @param phi
+     * @return
+     */
+    private int chooseLiteral(Set<Clause> phi)
     {
-        return false;
+        return phi.iterator()
+                  .next()
+                  .getLiterals()
+                  .iterator()
+                  .next();
+    }
+
+
+    /**
+     * Functie om de unit literal te vinden
+     * 
+     * @param phi
+     */
+    private int getUnitLiteral(Set<Clause> phi)
+    {
+        for (Clause x : phi) {
+            if (x.getLiterals().size() == 1) {
+                return x.getLiterals()
+                        .iterator()
+                        .next();
+            }
+        }
+
+        return 0;
     }
 }
